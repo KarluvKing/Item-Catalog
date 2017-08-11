6c83618c77c3c93057ff35528ccd4ba258b0d26d
Rui Paiva Branco <email@ruipaivabranco.com>
2017-08-11 12:18:21 +0100

    Database structure
    Database structure database_setup.py created with tables:
    user,
    categories,
    categorie_item

---
+++ database_setup.py
@@ -0,0 +1,53 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+class Categories(Base):
+    __tablename__ = 'categories'
+   
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+       	   'id'           	: self.id,
+       	   'title'         	: self.title,
+           'namdescription' : self.description,   
+       }
+
+class CategorieItem(Base):
+    __tablename__ = 'categorie_item'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(80), nullable=False)
+    description = Column(String(250))
+    categorie_id = Column(Integer, ForeignKey('categories.id'))
+    categorie = relationship(Categories)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+        	'id': self.id,
+            'title': self.name,
+            'description': self.description,
+        }
+
+engine = create_engine('sqlite:///catalogitem.db')
+
+
+Base.metadata.create_all(engine)
